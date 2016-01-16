3827378addba04231bf06d75a2e613e9d24c8eca
Tai Groot <groot.tai@gmail.com>
2016-01-16 11:08:32 -0800

    Update lib.js
    Folded key release code into releaseAllKeys()

--- lib.js
+++ lib.js
@@ -25,6 +25,13 @@ window.addShortcut = function(callback,keyCodeValues){
 window.addEventListener('keyup',function(e){
     keyStrokes[e.keyCode] = false;
 });
+function releaseAllKeys()
+{
+	for(var i=0;i< keyStrokes.length;i++)
+    {
+      keyStrokes[i] = false;
+    }
+}
 var key = {
     A:65,B:66,C:67,D:68,E:69,F:70,G:71,H:72,I:73,J:74,K:75,L:76,M:77,N:78,O:79,P:80,Q:81,R:82,S:83,T:84,U:85,V:86,W:87,X:88,Y:89,Z:90,
     ZERO:48,ONE:49,TWO:50,THREE:51,FOUR:52,FIVE:53,SIX:54,SEVEN:55,EIGHT:56,NINE:57,
